3c48492b7d94c51c70e571e29e80b5579697e874
Sam Stone <samstone920@googlemail.com>
2015-10-14 22:57:31 +0100

    initial commit

---
+++ Swifter/Swifter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swifter
+//
+//  Created by Sam Stone on 14/10/2015.
+//  Copyright (c) 2015 Sam Stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
